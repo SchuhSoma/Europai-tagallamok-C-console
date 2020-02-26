a70a924528af25d163fb033094a40ac68bf9723c
SchuhSoma <60043511+SchuhSoma@users.noreply.github.com>
2020-02-26 21:51:30 +0100

    Add files via upload

---
+++ EuropaiUnio/EuropaiUnio/EUtagallamok.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EuropaiUnio
+{
+    class EUtagallamok
+    {
+        public string EutagallamNeve;
+        public string CsatlakozasDatum;
+        public string Ev;
+        public string Honap;
+        public string Nap;
+
+        public EUtagallamok(string sor)
+        {
+            var dbok = sor.Split(';');
+            this.EutagallamNeve = dbok[0];
+            this.CsatlakozasDatum = dbok[1];
+            var dbok2 = CsatlakozasDatum.Split('.');
+            this.Ev = dbok2[0];
+            this.Honap= dbok2[1];
+            this.Nap = dbok2[2];
+        }
+    }
+}
